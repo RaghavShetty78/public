62f2e481a035390651fbb56e4dc029b3d3bee435
raghavshetty <raghavshetty319@gmail.com>
2022-03-04 08:00:36 +0000

    I have added some changes

--- branch.sh
+++ branch.sh
@@ -3,4 +3,5 @@
 git checkout -b new-branch
 git add .
 git commit -a -m "I have added some changes"
-git push -u "https://github.com/RaghavShetty78/public.git" main
+git push -u "https://github.com/RaghavShetty78/public.git" new-branch
+
